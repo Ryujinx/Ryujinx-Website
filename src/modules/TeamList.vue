b9bc4fefed652bbf8483fb485dbea865dbef2e89
Karim <20050570+yoshiyes@users.noreply.github.com>
2022-08-13 15:26:54 +0200

    Add team member GitHub link

--- src/modules/TeamList.vue
+++ src/modules/TeamList.vue
@@ -9,13 +9,12 @@ import { teamMembers } from "@/common/constants/teams";
     >
       <!-- Heading -->
       <div class="text-center">
-        <h2 class="text-3xl md:text-4xl font-extrabold mb-4">
-          Our team
-        </h2>
+        <h2 class="text-3xl md:text-4xl font-extrabold mb-4">Our team</h2>
         <h3
           class="text-lg md:text-xl md:leading-relaxed font-medium text-gray-600 lg:w-2/3 mx-auto"
         >
-          They are working nonstop behind the scenes to build the better Switch emulator.
+          They are working nonstop behind the scenes to build the better Switch
+          emulator.
         </h3>
       </div>
 
@@ -31,8 +30,14 @@ import { teamMembers } from "@/common/constants/teams";
               class="inline-block w-28 h-28 rounded-full"
             />
           </span>
-          <h4 class="text-xl font-semibold mb-1">{{ member.name }}</h4>
-          <p class="text-gray-600 font-medium">{{member.title}}</p>
+          <h4 class="text-xl font-semibold mb-1">
+            <a
+              class="text-blue-400"
+              :href="'https://github.com/' + member.githubName"
+              >{{ member.name }}
+            </a>
+          </h4>
+          <p class="text-gray-600 font-medium">{{ member.title }}</p>
         </div>
       </div>
     </div>
