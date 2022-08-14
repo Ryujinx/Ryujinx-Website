ec1eeeda3fb9cc9f2d5de9aa6df3ca115c6ee862
Karim <20050570+yoshiyes@users.noreply.github.com>
2022-08-14 11:45:48 +0200

    Add new stats homepage section

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -167,5 +167,66 @@ const DISCORD_URL = import.meta.env.VITE_DISCORD_URL;
     </div>
 
     <TeamList />
+
+    <!-- Stats -->
+    <!-- Stats Section: Bubble With Title -->
+    <div class="bg-gray-100 overflow-hidden">
+      <div class="container xl:max-w-7xl mx-auto px-4 py-16 lg:px-8 lg:py-32">
+        <!-- Heading -->
+        <div class="text-center mb-16">
+          <div
+            class="text-sm uppercase font-bold tracking-wider mb-1 text-blue-700"
+          >
+            {{t("views.homepage.supportUs")}}
+          </div>
+          <h2 class="text-3xl md:text-4xl font-extrabold mb-4">
+            {{t("views.homepage.trustedCommunities")}}
+          </h2>
+          <h3
+            class="text-lg md:text-xl md:leading-relaxed font-medium text-gray-600 lg:w-2/3 mx-auto"
+          >
+            {{t("views.homepage.trustedCommunitiesDescription")}}
+          </h3>
+        </div>
+
+        <!-- Stats -->
+        <div class="relative">
+          <div
+            class="pattern-dots-md text-gray-300 absolute top-0 right-0 w-32 h-32 transform -translate-y-12 translate-x-12"
+          ></div>
+          <div
+            class="pattern-dots-md text-gray-300 absolute bottom-0 left-0 w-32 h-32 transform translate-y-12 -translate-x-12"
+          ></div>
+          <div
+            class="grid grid-cols-1 sm:grid-cols-3 text-center rounded-xl shadow-md bg-white divide-y sm:divide-y-0 sm:divide-x divide-gray-100 overflow-hidden relative"
+          >
+            <dl
+              class="space-y-1 px-5 py-12 hover:bg-gray-50 hover:bg-opacity-50"
+            >
+              <dt class="text-4xl font-extrabold text-blue-600">12,5k+</dt>
+              <dd class="text-sm uppercase tracking-wide font-semibold">
+                {{t("views.homepage.githubStar")}}
+              </dd>
+            </dl>
+            <dl
+              class="space-y-1 px-5 py-12 hover:bg-gray-50 hover:bg-opacity-50"
+            >
+              <dt class="text-4xl font-extrabold text-blue-600">2,5k+</dt>
+              <dd class="text-sm uppercase tracking-wide font-semibold">
+                {{t("views.homepage.playableGames")}}
+              </dd>
+            </dl>
+            <dl
+              class="space-y-1 px-5 py-12 hover:bg-gray-50 hover:bg-opacity-50"
+            >
+              <dt class="text-4xl font-extrabold text-blue-600">450+</dt>
+              <dd class="text-sm uppercase tracking-wide font-semibold">
+                {{t("views.homepage.patreonContributors")}}
+              </dd>
+            </dl>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
