c99ae193e2e31cf256f3ea28114cbba1ed2cee8c
Karim <20050570+yoshiyes@users.noreply.github.com>
2022-08-17 16:58:26 +0200

    Add generic button component

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,11 @@ import { router } from "./routes";
 import { i18n } from "@/i18n";
 
 import App from "./App.vue";
+import GButton from "@/common/components/generic/GButton.vue";
 
-createApp(App).use(router).use(i18n).mount("#app");
+const app = createApp(App);
+
+app.use(router).use(i18n);
+app.component("GButton", GButton);
+
+app.mount("#app");
