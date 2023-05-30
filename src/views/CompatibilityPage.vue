40aba60f292019fb34db35a3c4986c936e07f8cb
Isaac Marovitz <isaacryu@icloud.com>
2023-05-30 17:59:30 -0400

    Create Compatibility Page

---
+++ src/views/CompatibilityPage.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import { useI18n } from "vue-i18n";
+
+const COMPATIBILITY_URL = import.meta.env.VITE_COMPATIBILITY_URL;
+const { t } = useI18n();
+</script>
+
+<template>
+</template>
