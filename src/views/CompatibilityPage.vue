bd26649188b95b8ff7eb6b213131de37dd3b7c0c
Isaac Marovitz <isaacryu@icloud.com>
2023-05-30 22:11:24 -0400

    Chart

--- src/views/CompatibilityPage.vue
+++ src/views/CompatibilityPage.vue
@@ -3,32 +3,41 @@ import { IssueSearch } from "@/types";
 import axios from "axios";
 import { onMounted, ref } from "vue";
 import { useI18n } from "vue-i18n";
+import { Doughnut } from "vue-chartjs";
+import { Chart, ArcElement, Title, Legend, Tooltip, Colors, ChartData } from 'chart.js'
+
+Chart.register(ArcElement, Title, Legend, Tooltip, Colors);
 
 const { t } = useI18n();
-const data = ref<PlayableTier[]>([
+const tierData = ref<PlayableTier[]>([
   {
     labelName: "status-playable",
     prettyPrintName: "Playable",
+    color: "green",
     count: 0
   },
   {
     labelName: "status-ingame",
     prettyPrintName: "In-Game",
+    color: "yellow",
     count: 0
   },
   {
     labelName: "status-menus",
     prettyPrintName: "Menus",
+    color: "yellow",
     count: 0
   },
   {
     labelName: "status-boots",
     prettyPrintName: "Boots",
+    color: "yellow",
     count: 0
   },
   {
     labelName: "status-nothing",
     prettyPrintName: "Nothing",
+    color: "red",
     count: 0
   },
 ]);
@@ -40,24 +49,53 @@ onMounted(() => {
 interface PlayableTier {
   labelName: string;
   prettyPrintName: string;
+  color: string;
   count: number;
 }
 
 const fetchStats = async () => {
   try {
-    data.value.forEach(async tier => {
+    await Promise.all(tierData.value.map(async tier => {
       const result = await axios.get<IssueSearch>(
         `${import.meta.env.VITE_LABEL_SEARCH_URL}label:${tier.labelName}+state:open`
       );
 
       tier.count = result.data.total_count;
-    });
-
-    console.log(data.value);
+    }));
+    
+    chartData.value = ({
+      labels: tierData.value.flatMap((tier) => tier.prettyPrintName),
+      datasets: [
+        {
+          data: tierData.value.flatMap((tier) => tier.count),
+          backgroundColor: tierData.value.flatMap((tier) => tier.color),
+        }
+      ]
+    })
   } catch (err) {
     console.error(err);
   }
 }
+
+const chartData = ref<ChartData<'doughnut'>>({
+  datasets: []
+})
+
 </script>
 
-<template></template>
+<template>
+  <div class="space-y-16 container xl:max-w-7xl mx-auto px-4 py-16 lg:px-8 lg:py-32">
+    <div class="text-center">
+      <h2 class="text-3xl md:text-4xl font-extrabold mb-4">
+        Compatibility
+      </h2>
+      <h3 class="text-lg md:text-xl md:leading-relaxed font-medium text-gray-600 lg:w-2/3 mx-auto">
+        {{ t("views.homepage.ourTeamDescription") }}
+      </h3>
+    </div>
+
+    <div class="container flex justify-center">
+      <Doughnut :data="chartData" />
+    </div>
+  </div>
+</template>
